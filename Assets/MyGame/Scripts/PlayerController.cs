04423de9f1dfaaa96ac73b1d07d5e5b70ebbc788
Twanghofer <tobias.w01@gmx.at>
2019-12-17 16:27:56 +0100

    unnötge namespaces entfernt, klammern angepasst, unnötige leerzeilen entfernt, alle deklarationen von variablen oben zusammengefasst

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator anim, anim2, anim3, anim4, anim5;
     [SerializeField] float jumpForce;
-    
+
+    bool grounded;
+    bool gameOver = false;
 
     private void Awake()
     {
@@ -15,13 +15,6 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0) && !gameOver && !gameOver && !gameOver)
@@ -30,24 +23,14 @@ public class PlayerController : MonoBehaviour
             {
                 jump();
             }
-
-
-
-
         }
     }
-
-    bool grounded;
-    bool gameOver = false;
 
     void jump()
     {
         grounded = false;
-
         rb.velocity = Vector2.up * jumpForce;
-
         anim.SetTrigger("Jump");
-
         GameManager.instance.IncrementScore();
         Debug.Log("DeleteMe");
     }
@@ -57,23 +40,22 @@ public class PlayerController : MonoBehaviour
         return true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)   {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         if(collision.gameObject.tag == "Ground")
         {
-            grounded = true;}
+            grounded = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle"){
+        if(collision.gameObject.tag == "Obstacle")
+        {
             GameManager.instance.GameOver();
             Destroy(collision.gameObject);
             anim.Play("SantaDeath");
             gameOver = SetGameOverTrue();
         }
     }
-
-
-
-
 }
